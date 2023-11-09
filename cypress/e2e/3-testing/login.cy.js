d74b3350980074990c69308e40a95a9edbfab65a
AdrianMikk <adrmik99436@stud.noroff.no>
2023-11-09 13:06:55 +0100

    Cypress testing update

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -1,22 +1,55 @@
 describe('Login', () => {
     it('should login an existing user', () => {
-        // Visit the login page
-        cy.visit('/');
-        cy.wait(1000);
-        cy.get('#registerForm').contains('Login').click();
-        cy.wait(1000);
-        cy.get('#loginEmail').type('adriantesting@stud.noroff.no');
-        cy.get('#loginPassword').type('adrianjs2');
-        cy.get('button[type=submit]').contains('Login').click();
+        cy.visit("/");
+        cy.wait(500);
+        cy.get("#registerModal").contains("Login").click();
+        cy.wait(500);
+        cy.get("#loginForm").should("be.visible");
+        cy.get("#loginEmail").type('adriantesting@stud.noroff.no');
+        cy.get("#loginPassword").type('adrianjs2');
+        cy.get("button[type=submit]").contains("Login").click();
     });
 
-    it('should deny user with invalid credentials', () => {
+    it('should deny empty input field', () => {
+        cy.visit("/");
+        cy.wait(500);
+        cy.get("#registerForm").contains("Login").click();
+        cy.wait(500);
+        cy.get("#loginForm").should("be.visible");
+        cy.get("button[type=submit]").contains("Login").click();
+        cy.get("#loginEmail:invalid").should("exist");
+    });
 
+    it('should deny empty password field', () => {
         cy.visit('/');
         cy.wait(1000);
         cy.get('#registerForm').contains('Login').click();
         cy.wait(1000);
-        cy.get('#loginEmail').type('adriantesting@gmail.com');
+        cy.get('#loginForm').should('be.visible');
+        cy.get('#loginEmail').type('adriantesting@stud.noroff.no');
+        cy.get('button[type=submit]').contains('Login').click();
+        cy.get('#loginPassword:invalid').should('exist');
+    });
+
+
+    it('should deny user with invalid email', () => {
+        cy.visit('/');
+        cy.wait(1000);
+        cy.get('#registerForm').contains('Login').click();
+        cy.wait(1000);
+        cy.get('#loginForm').should('be.visible');
+        cy.get('#loginEmail').type('adriantestinggmail.com');
+        cy.get('button[type=submit]').contains('Login').click();
+        cy.get('#loginEmail:invalid').should('exist');
+    });
+
+    it('should deny user with invalid password', () => {
+        cy.visit('/');
+        cy.wait(1000);
+        cy.get('#registerForm').contains('Login').click();
+        cy.wait(1000);
+        cy.get('#loginForm').should('be.visible');
+        cy.get('#loginEmail').type('adriantesting@stud.noroff.no')
         cy.get('#loginPassword').type('adrianjs');
         cy.get('button[type=submit]').contains('Login').click();
     });
