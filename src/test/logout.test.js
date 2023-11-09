dd632f72b2681a4dd68ce22e2466a4e755a53297
AdrianMikk <adrmik99436@stud.noroff.no>
2023-11-09 13:37:18 +0100

    Unit testing update

--- src/test/logout.test.js
+++ src/test/logout.test.js
@@ -1,11 +1,17 @@
 import { logout } from "../js/api/auth/logout.js";
-import * as storage from "../../storage/index.js";
 
-describe('Logout', () => {
-    it('Removes token from localStorage', () => {
-        const mockRemove = jest.spyOn(storage, 'remove');
+global.localStorage = {
+    removeItem: () => { },
+};
+
+describe("logout function", () => {
+    it("should remove the token from local storage", () => {
+        const removeItemMock = jest.spyOn(global.localStorage, 'removeItem').mockImplementation(() => { });
+
         logout();
-        expect(mockRemove).toHaveBeenCalledWith('token');
-        mockRemove.mockRestore();
+
+        expect(removeItemMock).toHaveBeenCalledWith("token");
+
+        removeItemMock.mockRestore();
     });
 });
