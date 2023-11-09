4d94460889fdc791bfbaaee6c6cf7899a673c435
AdrianMikk <adrmik99436@stud.noroff.no>
2023-11-09 13:51:36 +0100

    added consts to login.cy.js

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -1,20 +1,26 @@
+const workingEmail = 'adriantesting@stud.noroff.no';
+const workingPassword = 'adrianjs2';
+const invalidEmail = 'adriantestinggmail.com';
+const invalidPassword = 'adrianjs';
+
+
 describe('Login', () => {
-    it('should login an existing user', () => {
+    it('should access the profile when logged in', () => {
         cy.visit("/");
-        cy.wait(500);
+        cy.wait(1000);
         cy.get("#registerModal").contains("Login").click();
-        cy.wait(500);
+        cy.wait(1000);
         cy.get("#loginForm").should("be.visible");
-        cy.get("#loginEmail").type('adriantesting@stud.noroff.no');
-        cy.get("#loginPassword").type('adrianjs2');
-        cy.get("button[type=submit]").contains("Login").click();
+        cy.get("#loginEmail").type('workingEmail');
+        cy.get("#loginPassword").type('workingPassword');
+        cy.get('#loginForm > .modal-footer > .btn-success').contains('Login').click();
     });
 
     it('should deny empty input field', () => {
         cy.visit("/");
-        cy.wait(500);
+        cy.wait(1000);
         cy.get("#registerForm").contains("Login").click();
-        cy.wait(500);
+        cy.wait(1000);
         cy.get("#loginForm").should("be.visible");
         cy.get("button[type=submit]").contains("Login").click();
         cy.get("#loginEmail:invalid").should("exist");
@@ -26,7 +32,7 @@ describe('Login', () => {
         cy.get('#registerForm').contains('Login').click();
         cy.wait(1000);
         cy.get('#loginForm').should('be.visible');
-        cy.get('#loginEmail').type('adriantesting@stud.noroff.no');
+        cy.get('#loginEmail').type('workingEmail');
         cy.get('button[type=submit]').contains('Login').click();
         cy.get('#loginPassword:invalid').should('exist');
     });
@@ -38,7 +44,7 @@ describe('Login', () => {
         cy.get('#registerForm').contains('Login').click();
         cy.wait(1000);
         cy.get('#loginForm').should('be.visible');
-        cy.get('#loginEmail').type('adriantestinggmail.com');
+        cy.get('#loginEmail').type('invalidEmail');
         cy.get('button[type=submit]').contains('Login').click();
         cy.get('#loginEmail:invalid').should('exist');
     });
@@ -49,8 +55,8 @@ describe('Login', () => {
         cy.get('#registerForm').contains('Login').click();
         cy.wait(1000);
         cy.get('#loginForm').should('be.visible');
-        cy.get('#loginEmail').type('adriantesting@stud.noroff.no')
-        cy.get('#loginPassword').type('adrianjs');
+        cy.get('#loginEmail').type('workingEmail');
+        cy.get('#loginPassword').type('invalidPassword');
         cy.get('button[type=submit]').contains('Login').click();
     });
 });
